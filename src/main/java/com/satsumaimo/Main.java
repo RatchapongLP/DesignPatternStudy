4d3f80f2e9ee6db329065d9e7988d4bd035befa2
ratchapong <ratchapong.lpz@gmail.com>
2024-08-04 00:36:23 +0700

    add more testing

--- src/main/java/com/satsumaimo/Main.java
+++ src/main/java/com/satsumaimo/Main.java
@@ -1,14 +1,29 @@
 package com.satsumaimo;
 
+import com.satsumaimo.bean.Gender;
+import com.satsumaimo.bean.Mbti;
 import com.satsumaimo.bean.Person;
+import com.satsumaimo.creator.PrototypePerson;
 import com.satsumaimo.creator.SingletonPerson;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = SingletonPerson.getInstance();
-        System.out.println(person);
+        SingletonPerson singletonPerson = SingletonPerson.getInstance();
+        System.out.println(singletonPerson);
 
-        person = SingletonPerson.getInstance();
-        System.out.println(person);
+        singletonPerson = SingletonPerson.getInstance();
+        System.out.println(singletonPerson);
+
+        PrototypePerson prototypePerson = new PrototypePerson("Robert Pattinson", Gender.MALE, 38, Mbti.INFP, true);
+        System.out.println(prototypePerson);
+
+        Person cloned = prototypePerson.copyToMetaverse();
+        System.out.println(cloned);
+
+        cloned = prototypePerson.copyToMetaverse();
+        System.out.println(cloned);
+
+        cloned = prototypePerson.copyToMetaverse();
+        System.out.println(cloned);
     }
 }
