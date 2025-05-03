2001413704ab7966af22e71e559d5f9ca0be584c
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2025-05-04 02:43:13 +0700

    Create InputFactory

---
+++ src/main/java/com/satsumaimo/creational/InputFactory.java
@@ -0,0 +1,10 @@
+package com.satsumaimo.creational;
+
+public class InputFactory {
+    // This is the only changes.
+    // Suppose that every time a new implementation of Vehicle is released,
+    // another developer team does a new factory implementation to handle the released.
+    public static VehicleFactory[] getVehicleFactoryList() {
+        return new VehicleFactory[]{new SedanFactory(), new HatchbackFactory()};
+    }
+}
