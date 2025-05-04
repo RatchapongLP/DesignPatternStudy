2c861681b8b87e28036a4815407efae9e8ec0333
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2025-05-04 21:24:21 +0700

    Add CharacterFormatFactory class

---
+++ src/main/java/com/satsumaimo/structural/flyweight/CharacterFormatFactory.java
@@ -0,0 +1,17 @@
+package com.satsumaimo.structural.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Flyweight Factory: creates and reuses formatting instances
+ */
+public class CharacterFormatFactory {
+    private final Map<CharacterFormat, CharacterFormat> formats = new HashMap<>();
+
+    public CharacterFormat getFormat(String font, int size, boolean bold, boolean italic) {
+        CharacterFormat temp = new CharacterFormat(font, size, bold, italic);
+        formats.putIfAbsent(temp, temp); // Add if not present
+        return formats.get(temp); // Return shared instance
+    }
+}
