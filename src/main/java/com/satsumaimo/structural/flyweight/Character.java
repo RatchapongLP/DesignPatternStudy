c1ff804a1027b3237a01b80547a73697c22b3f69
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2025-05-04 21:24:05 +0700

    Add Character class

---
+++ src/main/java/com/satsumaimo/structural/flyweight/Character.java
@@ -0,0 +1,21 @@
+package com.satsumaimo.structural.flyweight;
+
+/**
+ * Character class: holds extrinsic state
+ */
+public class Character {
+    private final char character;
+    private final int position;
+    private final CharacterFormat format;
+
+    public Character(char character, int position, CharacterFormat format) {
+        this.character = character;
+        this.position = position;
+        this.format = format;
+    }
+
+    public void display() {
+        System.out.println("'" + character + "' at position " + position + " with format: " + format);
+    }
+}
+
