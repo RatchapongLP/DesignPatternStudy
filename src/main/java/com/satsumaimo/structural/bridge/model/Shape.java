0433b5ef6251836676564520678318b7b638448b
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2025-05-12 02:00:46 +0700

    Add a comment on Bridge pattern's solution to a problem

--- src/main/java/com/satsumaimo/structural/bridge/model/Shape.java
+++ src/main/java/com/satsumaimo/structural/bridge/model/Shape.java
@@ -1,5 +1,21 @@
 package com.satsumaimo.structural.bridge.model;
 
+/*
+When:
+
+                   ----Shape---
+                  /            \
+         Rectangle              Circle
+        /         \            /      \
+BlueRectangle  RedRectangle BlueCircle RedCircle
+
+Refactor to:
+
+          ----Shape---                        Color
+         /            \                       /   \
+Rectangle(Color)   Circle(Color)           Blue   Red
+        */
+
 public abstract class Shape {
     protected Outline outline;
     protected int corners;
