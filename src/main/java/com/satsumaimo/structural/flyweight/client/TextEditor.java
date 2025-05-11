c9c9d29601fcb4a227e6d22024c784769ea06d1d
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2025-05-12 01:53:10 +0700

    Delete unuseful code

--- src/main/java/com/satsumaimo/structural/flyweight/client/TextEditor.java
+++ src/main/java/com/satsumaimo/structural/flyweight/client/TextEditor.java
@@ -29,25 +29,13 @@ public class TextEditor {
             }
         }
 
-//        Runnable task = () -> {
-//            for (int i = 0; i < 1000; i++) {
-//                formatFactory.getFormat("Arial", 12, false, false); // all same style, should reuse
-//            }
-//        };
-//
-//        Thread t1 = new Thread(task);
-//        Thread t2 = new Thread(task);
-//
-//        t1.start(); t2.start();
-//        t1.join();  t2.join();
-
         // Expected = 1 shared object, but might be more!
         System.out.println("formatFactory.getFormatsSize() = " + formatFactory.getFormatsSize());
     }
 
     private static void runExample(int threadNumber) {
         List<Character> document = new ArrayList<>();
-
+   
         CharacterFormat regularFormat = formatFactory.getFormat("Arial", 12, false, false);
         String text = "Hello world";
 
