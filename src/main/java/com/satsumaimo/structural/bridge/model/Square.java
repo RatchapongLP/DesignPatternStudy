52c13be13823e0c4b2a43f552d9bfc5c26118380
Ratchapong Lertpun <ratchapong.lpz@gmail.com>
2025-05-12 01:54:31 +0700

    Add Bridge refined abstractions

---
+++ src/main/java/com/satsumaimo/structural/bridge/model/Square.java
@@ -0,0 +1,18 @@
+package com.satsumaimo.structural.bridge.model;
+
+public class Square extends Shape{
+    private static final int SQUARE_CORNERS = 4;
+    public Square(Outline outline) {
+        super(outline);
+        this.corners = SQUARE_CORNERS;
+    }
+
+    @Override
+    public void kickStart() {
+        System.out.println("Kick starting " + Square.class.getSimpleName());
+        System.out.println("I decided to draw meticulously..");
+        for (int i = 0; i < corners; i++) {
+            outline.draw();
+        }
+    }
+}
---
+++ src/main/java/com/satsumaimo/structural/bridge/model/Triangle.java
@@ -0,0 +1,17 @@
+package com.satsumaimo.structural.bridge.model;
+
+public class Triangle extends Shape{
+    private static final int TRIANGLE_CORNERS = 4;
+
+    public Triangle(Outline outline) {
+        super(outline);
+        this.corners = TRIANGLE_CORNERS;
+    }
+
+    @Override
+    public void kickStart() {
+        System.out.println("Kick starting " + Triangle.class.getSimpleName());
+        System.out.println("I decided to draw roughly!");
+        outline.draw();
+    }
+}
